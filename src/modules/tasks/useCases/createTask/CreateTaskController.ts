ba5e8386154280293259c4d584d6a8a52768ace7
fpdsjr <flaviopds.jr@gmail.com>
2022-05-15 03:44:48 -0300

    feat: implement CrateTaskController

---
+++ src/modules/tasks/useCases/createTask/CreateTaskController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+import { CreateTaskUseCase } from './CreateTaskUseCase'
+
+class CreateTaskController {
+  async handle (request: Request, response: Response): Promise<Response> {
+    const { description } = request.body
+
+    const createTaskUseCase = container.resolve(CreateTaskUseCase)
+
+    await createTaskUseCase.execute({ description })
+
+    return response.status(200).json('tasks created')
+  }
+}
+
+export { CreateTaskController }
