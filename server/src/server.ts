5fc3b3734d1640712641552fb59348e9e840c514
fpdsjr <flaviopds.jr@gmail.com>
2022-05-15 12:59:46 -0300

    refactor: change server port

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata'
 import './shared/container'
 import express from 'express'
+import { router } from './routes'
 
 const app = express()
 
 app.use(express.json())
 
-app.listen(3000, () => {
+app.use(router)
+
+app.listen(9090, () => {
   console.log('Server is running ')
 })
