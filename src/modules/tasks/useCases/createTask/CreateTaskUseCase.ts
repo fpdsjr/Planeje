47b0a5c37578d923498db7b57ff8e456b40eb122
fpdsjr <flaviopds.jr@gmail.com>
2022-05-15 03:39:04 -0300

    feat: implement CreateTaskUseCase

--- src/modules/tasks/useCases/createTask/CreateTaskUseCase.ts
+++ src/modules/tasks/useCases/createTask/CreateTaskUseCase.ts
@@ -1,7 +1,15 @@
+import { inject, injectable } from 'tsyringe'
+import { ICreateTaskDTO } from '../../dtos/ICreateTaskDTO'
+import { ITasksRepository } from '../../repositories/ITasksRepository'
 
+@injectable()
 class CreateTaskUseCase {
-  async execute (): Promise<void> {
+  constructor (
+    @inject('TasksRepository')
+    private readonly tasksRepository: ITasksRepository) {}
 
+  async execute ({ description }: ICreateTaskDTO): Promise<void> {
+    await this.tasksRepository.createTask({ description })
   }
 }
 
