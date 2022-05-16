f070d36e0fe696e56b375dccd7c259a4825c7574
fpdsjr <flaviopds.jr@gmail.com>
2022-05-15 22:47:44 -0300

    feat: set up server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,4 @@
-import 'reflect-metadata'
-import './shared/container'
-import express from 'express'
-import { router } from './routes'
-
-const app = express()
-
-app.use(express.json())
-
-app.use(router)
+import { app } from './app'
 
 app.listen(9090, () => {
   console.log('Server is running ')
