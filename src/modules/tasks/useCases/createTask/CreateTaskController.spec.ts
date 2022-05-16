a52b0e75de89b86cac51bd50f72e94a511bc4509
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 02:09:48 -0300

    refactor: remove async on tests

--- src/modules/tasks/useCases/createTask/CreateTaskController.spec.ts
+++ src/modules/tasks/useCases/createTask/CreateTaskController.spec.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import { app } from '../../../../app'
 
-describe('Create Task Controller', async () => {
+describe('Create Task Controller', () => {
   it('should be able to create a new task', async () => {
     const response = await request(app).post('/tasks/create').send({
       description: 'Test everything'
