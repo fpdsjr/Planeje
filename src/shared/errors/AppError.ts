22a3556ff888bb219108774d6ea8838f8802a97f
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 15:33:15 -0300

    refactor: "set error prototype"

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,9 +1,12 @@
-export class AppError {
+export class AppError extends Error {
   message: string
   statusCode: number
 
   constructor (message: string, statusCode = 400) {
+    super()
     this.message = message
     this.statusCode = statusCode
+
+    Object.setPrototypeOf(this, AppError.prototype)
   }
 }
