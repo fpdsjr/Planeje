4c5a136e91f4e39188a1b7404f6e64395b23e7f0
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 14:24:32 -0300

    feat: create deleteTask Route

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import { CreateTaskController } from '../modules/tasks/useCases/createTask/CreateTaskController'
+import { DeleteTaskController } from '../modules/tasks/useCases/deleteTask/DeleteTaskController'
 
 const tasksRoutes = Router()
 
 const createTaskController = new CreateTaskController()
+const deleteTaskController = new DeleteTaskController()
 
 tasksRoutes.post('/create', createTaskController.handle)
+
+tasksRoutes.post('/delete/:id', deleteTaskController.handle)
 
 export { tasksRoutes }
