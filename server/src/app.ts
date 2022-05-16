3112f8f9658b8c3ebb25b094454e07b4ceb58835
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 02:17:27 -0300

    chore: set up swagger

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,15 @@ import 'reflect-metadata'
 import './shared/container'
 import express from 'express'
 import { router } from './routes'
+import swaggerUI from 'swagger-ui-express'
+
+import swaggerDocs from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 app.use(router)
 
