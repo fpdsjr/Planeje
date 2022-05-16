e203e232285206691d6ea933d3d1b412637463e3
fpdsjr <flaviopds.jr@gmail.com>
2022-05-15 22:35:27 -0300

    chore: add uuid

--- server/src/modules/tasks/repositories/in-memory/TasksRepositoryInMemory.ts
+++ server/src/modules/tasks/repositories/in-memory/TasksRepositoryInMemory.ts
@@ -1,6 +1,7 @@
 import { Task, TasksStatus } from '@prisma/client'
 import { ICreateTaskDTO } from '../../dtos/ICreateTaskDTO'
 import { ITasksRepository } from '../ITasksRepository'
+import { v4 as uuidv4 } from 'uuid'
 
 class TasksRepositoryInMemory implements ITasksRepository {
   tasks: Task[] = []
