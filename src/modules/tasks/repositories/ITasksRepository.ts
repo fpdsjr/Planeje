fa1a200ee36309a14d7cff9f50752f142233fd30
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 15:36:04 -0300

    feat: add findTaskById

--- src/modules/tasks/repositories/ITasksRepository.ts
+++ src/modules/tasks/repositories/ITasksRepository.ts
@@ -1,8 +1,10 @@
+import { Task } from '@prisma/client'
 import { ICreateTaskDTO } from '../dtos/ICreateTaskDTO'
 
 interface ITasksRepository {
   createTask: ({ description }: ICreateTaskDTO) => Promise<void>
   deleteTask: (id: string) => Promise<void>
+  findTaskById: (id: string) => Promise<Task | null>
 }
 
 export { ITasksRepository }
