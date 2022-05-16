6d3bd2859368bfccaf8f5e1ddd5e222138191f3a
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 15:06:49 -0300

    chore: set up custom error

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata'
 import './shared/container'
-import express from 'express'
+import 'express-async-errors'
+import express, { Request, Response, NextFunction } from 'express'
 import { router } from './routes'
+import { AppError } from './shared/errors/AppError'
 import swaggerUI from 'swagger-ui-express'
 
 import swaggerDocs from './swagger.json'
@@ -13,5 +15,19 @@ app.use(express.json())
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 app.use(router)
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message
+      })
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: `internal server error - ${err.message}`
+    })
+  }
+)
 
 export { app }
