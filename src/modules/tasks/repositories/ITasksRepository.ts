3ce883c5ed4cd5e75d7e1fb93f0dcf58cf36afd5
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 14:24:24 -0300

    feat: create deleteTask

--- src/modules/tasks/repositories/ITasksRepository.ts
+++ src/modules/tasks/repositories/ITasksRepository.ts
@@ -2,6 +2,7 @@ import { ICreateTaskDTO } from '../dtos/ICreateTaskDTO'
 
 interface ITasksRepository {
   createTask: ({ description }: ICreateTaskDTO) => Promise<void>
+  deleteTask: (id: string) => Promise<void>
 }
 
 export { ITasksRepository }
