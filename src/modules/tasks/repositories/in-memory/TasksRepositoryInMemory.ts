f0355942551a12bc66a99fe57cf069dd96bb360b
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 19:49:36 -0300

    refactor: implement task entity

--- src/modules/tasks/repositories/in-memory/TasksRepositoryInMemory.ts
+++ src/modules/tasks/repositories/in-memory/TasksRepositoryInMemory.ts
@@ -1,19 +1,30 @@
-import { Task, TasksStatus } from '@prisma/client'
 import { ICreateTaskDTO } from '../../dtos/ICreateTaskDTO'
 import { ITasksRepository } from '../ITasksRepository'
-import { v4 as uuidv4 } from 'uuid'
+import { Task } from '../entities/Task'
 
 class TasksRepositoryInMemory implements ITasksRepository {
   tasks: Task[] = []
 
   async createTask ({ description }: ICreateTaskDTO): Promise<void> {
-    const task = {
-      id: uuidv4(),
-      description,
-      status: TasksStatus.PENDING
-    }
+    const task = new Task()
+
+    Object.assign(task, {
+      description
+    })
 
     this.tasks.push(task)
+  }
+
+  async findTaskById (id: string): Promise<Task | null> {
+    const findTaskById = this.tasks.find((task) => task.id === id)
+
+    return findTaskById as Task | null
+  }
+
+  async deleteTask (id: string): Promise<void> {
+    const deleteTaskById = this.tasks.filter((task) => task.id !== id)
+
+    deleteTaskById.map((task) => this.tasks.push(task))
   }
 }
 
