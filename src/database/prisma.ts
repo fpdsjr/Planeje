eb0e79ea9020698d5d5ac013ab1ad74ea8abde3d
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 18:12:27 -0300

    refactor: set up specific db test

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,3 +1,14 @@
+import 'dotenv/config'
+
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+const ENV = process.env.NODE_ENV ?? 'dev'
+
+const DB_URLS = {
+  dev: process.env.DATABASE_URL,
+  test: process.env.DATABASE_URL_TEST
+}
+
+const url = DB_URLS[ENV as keyof typeof DB_URLS]
+
+export const prisma = new PrismaClient({ datasources: { db: { url } } })
