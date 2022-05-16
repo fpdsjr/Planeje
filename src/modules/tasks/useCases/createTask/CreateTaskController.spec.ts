6bcb47108ed883f3a788fbf2af6ee6b0876b2726
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 01:46:01 -0300

    test: add CreateTaskController

---
+++ src/modules/tasks/useCases/createTask/CreateTaskController.spec.ts
@@ -0,0 +1,12 @@
+import request from 'supertest'
+import { app } from '../../../../app'
+
+describe('Create Task Controller', async () => {
+  it('should be able to create a new task', async () => {
+    const response = await request(app).post('/tasks/create').send({
+      description: 'Test everything'
+    })
+
+    expect(response.status).toBe(201)
+  })
+})
