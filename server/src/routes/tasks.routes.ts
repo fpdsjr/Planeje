4938fb8417f53ea434959118ddf9dde4fc5be403
fpdsjr <flaviopds.jr@gmail.com>
2022-05-17 12:14:19 -0300

    feat: add update task status route

--- server/src/routes/tasks.routes.ts
+++ server/src/routes/tasks.routes.ts
@@ -2,17 +2,21 @@ import { Router } from 'express'
 import { CreateTaskController } from '../modules/tasks/useCases/createTask/CreateTaskController'
 import { DeleteTaskController } from '../modules/tasks/useCases/deleteTask/DeleteTaskController'
 import { UpdateTaskController } from '../modules/tasks/useCases/updateTask/UpdateTaskController'
+import { UpdateTaskStatusController } from '../modules/tasks/useCases/updateTaskStatus/UpdateTaskStatusController'
 
 const tasksRoutes = Router()
 
 const createTaskController = new CreateTaskController()
 const deleteTaskController = new DeleteTaskController()
 const updateTaskController = new UpdateTaskController()
+const updateTaskStatusController = new UpdateTaskStatusController()
 
 tasksRoutes.post('/create', createTaskController.handle)
 
 tasksRoutes.delete('/delete/:id', deleteTaskController.handle)
 
 tasksRoutes.put('/update/:id', updateTaskController.handle)
+
+tasksRoutes.patch('/update/status/:id', updateTaskStatusController.handle)
 
 export { tasksRoutes }
