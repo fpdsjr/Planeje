51798c56b74160face6cc9f320e4a440e4a7af4e
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 21:18:29 -0300

    test: add DeleteTaskController

---
+++ server/src/modules/tasks/useCases/deleteTask/DeleteTaskController.spec.ts
@@ -0,0 +1,18 @@
+import request from 'supertest'
+import { app } from '../../../../app'
+import { Task } from '../../repositories/entities/Task'
+
+describe('Delete Task Controller', () => {
+  it('should be able to delete a task', async () => {
+    const createdTask = await request(app).post('/tasks/create').send({
+      description: 'Be rich'
+    })
+
+    const { id } = createdTask.body as Task
+
+    const deleteTask = await request(app).delete(`/tasks/delete/${id}`)
+
+    expect(deleteTask.status).toBe(200)
+    expect(deleteTask.body.message).toEqual('Task Deleted')
+  })
+})
