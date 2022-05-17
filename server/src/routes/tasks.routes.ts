b934e067903da2b59a008a37512c74c8a516dd25
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 23:32:39 -0300

    feat: create update Route

--- server/src/routes/tasks.routes.ts
+++ server/src/routes/tasks.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express'
 import { CreateTaskController } from '../modules/tasks/useCases/createTask/CreateTaskController'
 import { DeleteTaskController } from '../modules/tasks/useCases/deleteTask/DeleteTaskController'
+import { UpdateTaskController } from '../modules/tasks/useCases/updateTask/UpdateTaskController'
 
 const tasksRoutes = Router()
 
 const createTaskController = new CreateTaskController()
 const deleteTaskController = new DeleteTaskController()
+const updateTaskController = new UpdateTaskController()
 
 tasksRoutes.post('/create', createTaskController.handle)
 
 tasksRoutes.delete('/delete/:id', deleteTaskController.handle)
+
+tasksRoutes.put('/update/:id', updateTaskController.handle)
 
 export { tasksRoutes }
