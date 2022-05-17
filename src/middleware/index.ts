43f86001ef400586d62a567fa658576118c7cd98
fpdsjr <flaviopds.jr@gmail.com>
2022-05-17 19:58:25 -0300

    refactor: add ErrorHadle as middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express'
+import { AppError } from '../shared/errors/AppError'
+
+export async function ErrorHandle (err: Error, request: Request, response: Response, next: NextFunction): Promise<Response> {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: `internal server error - ${err.message}`
+  })
+}
