74320decd8d70454eb707c975832700f38efdd75
fpdsjr <flaviopds.jr@gmail.com>
2022-05-17 17:47:03 -0300

    feat: add listallltasks route

--- server/src/routes/tasks.routes.ts
+++ server/src/routes/tasks.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { CreateTaskController } from '../modules/tasks/useCases/createTask/CreateTaskController'
 import { DeleteTaskController } from '../modules/tasks/useCases/deleteTask/DeleteTaskController'
+import { ListAllTasksController } from '../modules/tasks/useCases/listAllTasks/ListAllTasksController'
 import { UpdateTaskController } from '../modules/tasks/useCases/updateTask/UpdateTaskController'
 import { UpdateTaskStatusController } from '../modules/tasks/useCases/updateTaskStatus/UpdateTaskStatusController'
 
@@ -10,6 +11,7 @@ const createTaskController = new CreateTaskController()
 const deleteTaskController = new DeleteTaskController()
 const updateTaskController = new UpdateTaskController()
 const updateTaskStatusController = new UpdateTaskStatusController()
+const listAllTasksController = new ListAllTasksController()
 
 tasksRoutes.post('/create', createTaskController.handle)
 
@@ -18,5 +20,7 @@ tasksRoutes.delete('/delete/:id', deleteTaskController.handle)
 tasksRoutes.put('/update/:id', updateTaskController.handle)
 
 tasksRoutes.patch('/update/status/:id', updateTaskStatusController.handle)
+
+tasksRoutes.get('/list', listAllTasksController.handle)
 
 export { tasksRoutes }
