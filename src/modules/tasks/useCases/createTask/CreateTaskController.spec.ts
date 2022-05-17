e2a79d7cf989e1c80a35d1634ce980d1fb3fcbe8
fpdsjr <flaviopds.jr@gmail.com>
2022-05-17 19:50:47 -0300

    test: apply prisma delete after tests

--- src/modules/tasks/useCases/createTask/CreateTaskController.spec.ts
+++ src/modules/tasks/useCases/createTask/CreateTaskController.spec.ts
@@ -1,7 +1,13 @@
+
 import request from 'supertest'
 import { app } from '../../../../app'
+import { prisma } from '../../../../database/prisma'
 
 describe('Create Task Controller', () => {
+  afterAll(async () => {
+    await prisma.task.deleteMany()
+  })
+
   it('should be able to create a new task', async () => {
     const response = await request(app).post('/tasks/create').send({
       description: 'Test everything'
--- src/modules/tasks/useCases/deleteTask/DeleteTaskController.spec.ts
+++ src/modules/tasks/useCases/deleteTask/DeleteTaskController.spec.ts
@@ -1,8 +1,14 @@
+
 import request from 'supertest'
 import { app } from '../../../../app'
+import { prisma } from '../../../../database/prisma'
 import { Task } from '../../repositories/entities/Task'
 
 describe('Delete Task Controller', () => {
+  afterAll(async () => {
+    await prisma.task.deleteMany()
+  })
+
   it('should be able to delete a task', async () => {
     const createdTask = await request(app).post('/tasks/create').send({
       description: 'Be rich'
