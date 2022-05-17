90c3df0575e00f9fc7e23da6927f9eec24224a96
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 23:38:28 -0300

    test: add UpdateTaskController

---
+++ server/src/modules/tasks/useCases/updateTask/UpdateTaskController.spec.ts
@@ -0,0 +1,20 @@
+import request from 'supertest'
+import { app } from '../../../../app'
+import { Task } from '../../repositories/entities/Task'
+
+describe('Update Task Controller ', () => {
+  it('should be able to update a task ', async () => {
+    const createTask = await request(app).post('/tasks/create').send({
+      description: 'And I feel blessed'
+    })
+
+    const { id } = createTask.body as Task
+
+    const updateTask = await request(app).put(`/tasks/update/${id}`).send({
+      description: 'Updated task'
+    })
+
+    expect(updateTask.status).toBe(200)
+    expect(updateTask.body.description).toEqual('Updated task')
+  })
+})
