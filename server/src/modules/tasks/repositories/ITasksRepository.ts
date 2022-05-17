d102f10a742c5ac596342626981a8354af22a0f7
fpdsjr <flaviopds.jr@gmail.com>
2022-05-17 12:15:52 -0300

    feat: implement updateTaskStatus

--- server/src/modules/tasks/repositories/ITasksRepository.ts
+++ server/src/modules/tasks/repositories/ITasksRepository.ts
@@ -1,12 +1,14 @@
 import { Task } from '@prisma/client'
 import { ICreateTaskDTO } from '../dtos/ICreateTaskDTO'
 import { IUpdateTaskDTO } from '../dtos/IUpdateTaskDTO'
+import { IUpdateTaskStatusDTO } from '../dtos/IUpdateTaskStatusDTO'
 
 interface ITasksRepository {
   createTask: ({ description }: ICreateTaskDTO) => Promise<Task>
   deleteTask: (id: string) => Promise<void>
   findTaskById: (id: string) => Promise<Task | null>
   updateTask: ({ id, description }: IUpdateTaskDTO) => Promise<Task>
+  updateTaskStatus: ({ id, status }: IUpdateTaskStatusDTO) => Promise<Task>
 }
 
 export { ITasksRepository }
