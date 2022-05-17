1ff4b5c36fe0db877c26f6f39cb5fbea4d5afb22
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 21:19:35 -0300

    refactor: change interface return

--- server/src/modules/tasks/repositories/ITasksRepository.ts
+++ server/src/modules/tasks/repositories/ITasksRepository.ts
@@ -2,7 +2,7 @@ import { Task } from '@prisma/client'
 import { ICreateTaskDTO } from '../dtos/ICreateTaskDTO'
 
 interface ITasksRepository {
-  createTask: ({ description }: ICreateTaskDTO) => Promise<void>
+  createTask: ({ description }: ICreateTaskDTO) => Promise<Task>
   deleteTask: (id: string) => Promise<void>
   findTaskById: (id: string) => Promise<Task | null>
 }
