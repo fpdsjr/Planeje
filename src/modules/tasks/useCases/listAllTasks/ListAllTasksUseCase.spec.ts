ebb365ed56e94fabe7d83e4097fe74c98712152f
fpdsjr <flaviopds.jr@gmail.com>
2022-05-17 18:46:45 -0300

    test: add ListAllTasksUseCase

---
+++ src/modules/tasks/useCases/listAllTasks/ListAllTasksUseCase.spec.ts
@@ -0,0 +1,34 @@
+import { TasksRepositoryInMemory } from '../../repositories/in-memory/TasksRepositoryInMemory'
+import { ListAllTasksUseCase } from './ListAllTasksUseCase'
+
+let tasksRepositoryInMemory: TasksRepositoryInMemory
+let listAllTasksUseCase: ListAllTasksUseCase
+
+describe('List All Tasks ', () => {
+  beforeEach(() => {
+    tasksRepositoryInMemory = new TasksRepositoryInMemory()
+    listAllTasksUseCase = new ListAllTasksUseCase(tasksRepositoryInMemory)
+  })
+
+  afterAll(() => {
+    tasksRepositoryInMemory.tasks.length = 0
+  })
+
+  it('should be able to list all tasks', async () => {
+    await tasksRepositoryInMemory.createTask({
+      description: 'Go to the gym'
+    })
+
+    await tasksRepositoryInMemory.createTask({
+      description: 'Go to the store'
+    })
+
+    await tasksRepositoryInMemory.createTask({
+      description: 'Go to the market'
+    })
+
+    const listAllTasks = await listAllTasksUseCase.execute()
+
+    expect(listAllTasks.length).toBe(3)
+  })
+})
