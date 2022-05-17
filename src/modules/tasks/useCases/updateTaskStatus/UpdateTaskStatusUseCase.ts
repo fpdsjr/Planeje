ebe3bebf337cd9b4390977d3a7a98739ebc4960c
fpdsjr <flaviopds.jr@gmail.com>
2022-05-17 12:14:31 -0300

    feat: add UpdateTaskStatusUseCase

---
+++ src/modules/tasks/useCases/updateTaskStatus/UpdateTaskStatusUseCase.ts
@@ -0,0 +1,33 @@
+import { TasksStatus } from '@prisma/client'
+import { inject, injectable } from 'tsyringe'
+import { AppError } from '../../../../shared/errors/AppError'
+import { IUpdateTaskStatusDTO } from '../../dtos/IUpdateTaskStatusDTO'
+import { Task } from '../../repositories/entities/Task'
+import { ITasksRepository } from '../../repositories/ITasksRepository'
+
+@injectable()
+class UpdateTaskStatusUseCase {
+  constructor (
+    @inject('TasksRepository')
+    private readonly tasksRepository: ITasksRepository) { }
+
+  async execute ({ id, status }: IUpdateTaskStatusDTO): Promise<Task> {
+    const findTask = this.tasksRepository.findTaskById(id)
+
+    if (!findTask) {
+      throw new AppError('Task do not exists!', 404)
+    }
+
+    const values = Object.values(TasksStatus)
+
+    if (!values.includes(status)) {
+      throw new AppError('Invalid Status', 403)
+    }
+
+    const updatedTaskStatus = await this.tasksRepository.updateTaskStatus({ id, status })
+
+    return updatedTaskStatus
+  }
+}
+
+export { UpdateTaskStatusUseCase }
