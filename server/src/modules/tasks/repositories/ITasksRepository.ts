c47094f891e991cd702d167359922f146e5ec25b
fpdsjr <flaviopds.jr@gmail.com>
2022-05-16 21:41:33 -0300

    feat: add updateTask

--- server/src/modules/tasks/repositories/ITasksRepository.ts
+++ server/src/modules/tasks/repositories/ITasksRepository.ts
@@ -1,10 +1,12 @@
 import { Task } from '@prisma/client'
 import { ICreateTaskDTO } from '../dtos/ICreateTaskDTO'
+import { IUpdateTaskDTO } from '../dtos/IUpdateTaskDTO'
 
 interface ITasksRepository {
   createTask: ({ description }: ICreateTaskDTO) => Promise<Task>
   deleteTask: (id: string) => Promise<void>
   findTaskById: (id: string) => Promise<Task | null>
+  updateTask: ({ id, description }: IUpdateTaskDTO) => Promise<Task>
 }
 
 export { ITasksRepository }
