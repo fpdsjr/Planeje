ca0177c0fe0dba91ea31026efad64b534a06bc55
fpdsjr <flaviopds.jr@gmail.com>
2022-05-18 17:25:28 -0300

    feat: add Header

---
+++ web/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Content } from './styles';
+
+const Header = () => {
+  return (
+    <Container>
+      <Content>
+        <h1>Planeje</h1>
+      </Content>
+    </Container>
+  );
+};
+
+export default Header;
