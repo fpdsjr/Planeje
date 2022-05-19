76d13cecff3739918ead99da0cf2987edba44105
fpdsjr <flaviopds.jr@gmail.com>
2022-05-19 18:52:37 -0300

    feat: import Main

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,13 @@ import { GlobalStyle } from './styles/global';
 
 import { Container } from './styles/';
 import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <Container>
       <Header />
+      <Main />
       <GlobalStyle />
     </Container>
   );
