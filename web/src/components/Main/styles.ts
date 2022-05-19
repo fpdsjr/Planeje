0763c7daedd860dd483fca092776f06bfc4ba0f8
fpdsjr <flaviopds.jr@gmail.com>
2022-05-19 18:53:29 -0300

    refactor: change main styles

--- web/src/components/Main/styles.ts
+++ web/src/components/Main/styles.ts
@@ -1,19 +1,47 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 950px;
-  height: 900px;
+  width: 800px;
+  height: 280px;
   position: relative;
-  bottom: 140px;
+  bottom: 180px;
   margin: 0 auto;
   display: flex;
   align-items: center;
   flex-direction: column;
   gap: 2rem;
+
+  footer {
+    position: relative;
+    width: 700px;
+    border-radius: 0px 0 0.25rem 0.25rem;
+    background-color: var(--veryDarkDesaturatedBlue);
+    padding: 1.3rem;
+    top: -32px;
+
+    div {
+      display: flex;
+      gap: 8rem;
+      align-items: center;
+      justify-content: center;
+
+      button {
+        border: none;
+        background-color: var(--veryDarkDesaturatedBlue);
+        color: var(--lightGrayishBlue);
+        transition: all 0.2s ease-in-out;
+
+        &:hover {
+          color: var(--veryDarkGrayishBlueHover);
+          transform: scale(1.05);
+        }
+      }
+    }
+  }
 `;
 
 export const CreateTaskInput = styled.input`
-  width: 850px;
+  width: 700px;
   height: 60px;
   border-radius: 0.25rem;
   background-color: var(--veryDarkDesaturatedBlue);
@@ -28,7 +56,8 @@ export const CreateTaskInput = styled.input`
 `;
 
 export const TasksContainer = styled.div`
-  width: 850px;
+  position: relative;
+  width: 700px;
   height: 500px;
   background-color: var(--veryDarkDesaturatedBlue);
   border-radius: 0.25rem;
