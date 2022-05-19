66e7353a4c28f8ee4162fc3cbcb7d7ad0684951f
fpdsjr <flaviopds.jr@gmail.com>
2022-05-18 21:22:58 -0300

    feat: set up cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,12 +5,19 @@ import express from 'express'
 import { router } from './routes'
 import { ErrorHandle } from './middleware'
 import swaggerUI from 'swagger-ui-express'
+import cors from 'cors'
 
 import swaggerDocs from '../../../swagger.json'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000'
+  })
+)
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
