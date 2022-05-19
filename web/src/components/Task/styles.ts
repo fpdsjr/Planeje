234bcceacec68c23a0675e3278bf830438f6f996
fpdsjr <flaviopds.jr@gmail.com>
2022-05-19 18:53:59 -0300

    refactor: change Task styles

--- web/src/components/Task/styles.ts
+++ web/src/components/Task/styles.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
+interface IRequest {
+  toggleEditor: boolean;
+}
+
 export const Container = styled.div`
+  position: relative;
+
   hr {
     border: none;
     height: 1px;
@@ -9,7 +15,8 @@ export const Container = styled.div`
 `;
 
 export const ContentContainer = styled.div`
-  width: 850px;
+  width: 700px;
+  overflow: hidden;
   height: 60px;
   border-radius: 0.25rem;
   background-color: var(--veryDarkDesaturatedBlue);
@@ -20,11 +27,20 @@ export const ContentContainer = styled.div`
   padding: 1rem;
   color: white;
 
-  div {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
+  select {
+    background-color: var(--veryDarkDesaturatedBlue);
+    color: white;
+    width: 90px;
+    height: 30px;
+    border: none;
+    border-radius: 0.25rem;
   }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
 
   select {
     background-color: var(--veryDarkDesaturatedBlue);
@@ -33,5 +49,61 @@ export const ContentContainer = styled.div`
     height: 30px;
     border: none;
     border-radius: 0.25rem;
+
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      transform: scale(1.2);
+      filter: brightness(0.5);
+    }
+  }
+
+  div {
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      transform: scale(1.2);
+      filter: brightness(0.5);
+    }
+  }
+`;
+
+export const TaskEditor = styled.div<IRequest>`
+  opacity: ${(props) => (props.toggleEditor ? '1' : '0')};
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  input {
+    width: 400px;
+    height: 40px;
+    border-radius: 0.25rem;
+    background-color: var(--veryDarkBlue);
+    border: none;
+    font-size: 18px;
+    color: white;
+    padding: 0.5rem;
+    margin-right: 10px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  button {
+    border: none;
+    color: white;
+    border-radius: 0.25rem;
+    height: 40px;
+    padding: 0.5rem;
+    font-size: 16px;
+    background-color: var(--veryDarkBlue);
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      background-color: var(--veryDarkGrayishBlue);
+      transform: scale(1.05);
+    }
   }
 `;
