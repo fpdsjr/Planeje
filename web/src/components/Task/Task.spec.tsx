d94e7ae21fb7c15b351aa02f22c9faf1d8d27159
fpdsjr <flaviopds.jr@gmail.com>
2022-05-20 11:10:04 -0300

    test: add Task test

---
+++ web/src/components/Task/Task.spec.tsx
@@ -0,0 +1,100 @@
+import { fireEvent, render } from '@testing-library/react';
+import Task from '.';
+import fetchApi from '../../utils/fetchApi';
+
+jest.mock('../../utils/fetchApi');
+
+const deleteTask = jest.fn();
+const setIsHavingATaskEvent = jest.fn();
+
+describe('Task description', () => {
+  it('should change the status', () => {
+    const { getByTestId, getByText } = render(
+      <Task
+        id="123456"
+        description="new task add"
+        status="PENDING"
+        deleteTask={deleteTask}
+        setIsHavingATaskEvent={setIsHavingATaskEvent}
+        isHavingATaskEvent={true}
+      />,
+    );
+
+    (fetchApi as jest.Mock).mockReturnValue('OK');
+
+    const select = getByTestId('select-option');
+
+    fireEvent.change(select, {
+      target: { value: 'DONE' },
+    });
+
+    expect(fetchApi).toHaveBeenCalled();
+  });
+
+  it('should change the status', () => {
+    const { getByTestId } = render(
+      <Task
+        id="123456"
+        description="new task add"
+        status="PENDING"
+        deleteTask={deleteTask}
+        setIsHavingATaskEvent={setIsHavingATaskEvent}
+        isHavingATaskEvent={true}
+      />,
+    );
+
+    (fetchApi as jest.Mock).mockReturnValue('OK');
+
+    const select = getByTestId('select-option');
+
+    fireEvent.change(select, {
+      target: { value: 'DONE' },
+    });
+
+    expect(fetchApi).toHaveBeenCalled();
+  });
+
+  it('should update a test update a task', () => {
+    const { getByText } = render(
+      <Task
+        id="123456"
+        description="new task add"
+        status="PENDING"
+        deleteTask={deleteTask}
+        setIsHavingATaskEvent={setIsHavingATaskEvent}
+        isHavingATaskEvent={true}
+      />,
+    );
+
+    (fetchApi as jest.Mock).mockReturnValue('OK');
+
+    const select = getByText('Atualizar');
+
+    fireEvent.click(select);
+
+    expect(fetchApi).toHaveBeenCalled();
+  });
+
+  it('should delete a task', () => {
+    const deleteTask = jest.fn();
+
+    const { getByTestId } = render(
+      <Task
+        id="123456"
+        description="new task add"
+        status="PENDING"
+        deleteTask={deleteTask}
+        setIsHavingATaskEvent={setIsHavingATaskEvent}
+        isHavingATaskEvent={true}
+      />,
+    );
+
+    (fetchApi as jest.Mock).mockReturnValue('OK');
+
+    const select = getByTestId('delete-task');
+
+    fireEvent.click(select);
+
+    expect(deleteTask).toHaveBeenCalled();
+  });
+});
