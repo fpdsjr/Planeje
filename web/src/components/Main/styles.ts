c9b7df8fd6731d566ece1a21f4078a6ba21fbc0b
fpdsjr <flaviopds.jr@gmail.com>
2022-05-24 15:21:11 -0300

    feat:  add Main style

--- web/src/components/Main/styles.ts
+++ web/src/components/Main/styles.ts
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+interface ITaskCont {
+  taskCont: number;
+}
+
+export const Container = styled.div<ITaskCont>`
   width: 800px;
-  height: 280px;
+  height: 230px;
   position: relative;
   bottom: 180px;
   margin: 0 auto;
@@ -11,30 +15,51 @@ export const Container = styled.div`
   flex-direction: column;
   gap: 2rem;
 
+  @media (max-width: 800px) {
+    width: 380px;
+  }
+
   footer {
+    display: ${(props) => (props.taskCont > 0 ? '1' : 'none !important')};
     position: relative;
     width: 700px;
     border-radius: 0px 0 0.25rem 0.25rem;
     background-color: var(--veryDarkDesaturatedBlue);
     padding: 1.3rem;
     top: -32px;
+    background-image: linear-gradient(
+      to right top,
+      #721d7b,
+      #6c1d85,
+      #642090,
+      #58239b,
+      #4628a6
+    );
+
+    @media (max-width: 800px) {
+      width: 380px;
+    }
 
     div {
       display: flex;
-      gap: 8rem;
+      gap: 4rem;
       align-items: center;
       justify-content: center;
 
       button {
         border: none;
-        background-color: var(--veryDarkDesaturatedBlue);
+        background: none;
         color: var(--lightGrayishBlue);
         transition: all 0.2s ease-in-out;
 
         &:hover {
-          color: var(--veryDarkGrayishBlueHover);
+          color: var(--veryDarkBlue);
           transform: scale(1.05);
         }
+      }
+
+      @media (max-width: 800px) {
+        gap: 3rem;
       }
     }
   }
@@ -53,14 +78,23 @@ export const CreateTaskInput = styled.input`
   &:focus {
     outline: none;
   }
+
+  @media (max-width: 800px) {
+    width: 380px;
+  }
 `;
 
-export const TasksContainer = styled.div`
+export const TasksContainer = styled.div<ITaskCont>`
+  display: ${(props) => (props.taskCont > 0 ? '1' : 'none !important')};
   position: relative;
   width: 700px;
   height: 500px;
   background-color: var(--veryDarkDesaturatedBlue);
   border-radius: 0.25rem;
+
+  @media (max-width: 800px) {
+    width: 380px;
+  }
 `;
 
 export const ErrorComponent = styled.div`
