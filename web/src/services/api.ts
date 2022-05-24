9ca7c0610643fba4b80a44f8b8d19c183e5e268d
fpdsjr <flaviopds.jr@gmail.com>
2022-05-23 23:58:33 -0300

    feat: add localhost

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const Api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3333/",   
 });
