827492574eda774aaa3cce614be568b8d77429b4
fpdsjr <flaviopds.jr@gmail.com>
2022-05-24 15:20:58 -0300

    feat: add Header styles

--- web/src/components/Header/styles.ts
+++ web/src/components/Header/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import bgDesktopDark from '../../assets/bg-desktop-dark.jpg';
+import bgMobileDark from '../../assets/bg-mobile-dark.jpg';
 
 export const Container = styled.div`
   width: 100%;
@@ -8,15 +9,24 @@ export const Container = styled.div`
   background-size: cover;
   display: flex;
   align-items: center;
+
+  @media (max-width: 540px) {
+    background-image: url(${bgMobileDark});
+  }
 `;
 
 export const Content = styled.div`
   margin: 0 auto;
-  width: 700px;
+  width: 100%;
   height: 300px;
   display: flex;
   margin-top: -50px;
   align-items: center;
+  justify-content: center;
   color: var(--lightGrayishBlue);
   text-transform: uppercase;
+
+  @media (max-width: 800px) {
+    width: 380px;
+  }
 `;
